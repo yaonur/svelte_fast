4db64a8ba5525522dfe2abb6ee9314131a3fbca3
ynr <ya_onur@hotmail.com>
2023-02-01 19:20:10 +0300

    ss

--- sveltekit/consultingninja/load/src/routes/+page.ts
+++ sveltekit/consultingninja/load/src/routes/+page.ts
@@ -1,7 +1,12 @@
 export async function load({ fetch }) {
-  const resp = await fetch("http://randomuser.me/api/");
-  const data = await resp.json();
+  const fetchPersons = async () => {
+    const resp = await fetch("http://randomuser.me/api/");
+    const data = await resp.json();
+    return {
+      data
+    };
+  };
   return {
-    data
+    users: fetchPersons()
   };
 }
