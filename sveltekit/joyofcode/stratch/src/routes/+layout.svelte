f541ca88688108baef899e8685c2465ad890697d
ynr <ya_onur@hotmail.com>
2023-02-01 08:50:45 +0300

    ss

---
+++ sveltekit/joyofcode/stratch/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '@picocss/pico';
+	import '../app.css';
+</script>
+
+<body data-theme="dark">
+	<slot />
+</body>
+
+<style>
+	body{
+		height: 100vh;
+		background-color: var(--background-color);
+	}
+</style>
---
+++ sveltekit/joyofcode/stratch/src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<nav class="container-fluid">
+	<ul>
+		<li><a href="./" class="contrast" onclick="event.preventDefault()"><strong>Brand</strong></a></li>
+	</ul>
+	<ul>
+		<li>
+			<details role="list" dir="rtl">
+				<summary aria-haspopup="listbox" role="link" class="secondary">Theme</summary>
+				<ul role="listbox">
+					<li><a href="#" data-theme-switcher="auto">Auto</a></li>
+					<li><a href="#" data-theme-switcher="light">Light</a></li>
+					<li><a href="#" data-theme-switcher="dark">Dark</a></li>
+				</ul>
+			</details>
+		</li>
+		<li>
+			<details role="list" dir="rtl">
+				<summary aria-haspopup="listbox" role="link" class="secondary">Examples</summary>
+				<ul role="listbox">
+					<li><a href="../preview/">Preview</a></li>
+					<li><a href="../preview-rtl/">Right-to-left</a></li>
+					<li><a href="../classless/">Class-less</a></li>
+					<li><a href="../basic-template/">Basic template</a></li>
+					<li><a href="../company/">Company</a></li>
+					<li><a href="../google-amp/">Google Amp</a></li>
+					<li><a href="../sign-in/">Sign in</a></li>
+					<li><a href="../bootstrap-grid/">Bootstrap grid</a></li>
+				</ul>
+			</details>
+		</li>
+	</ul>
+</nav><!-- ./ Nav -->
+
+      <!-- Main -->
+<main class="container">
+	<article class="grid">
+		<div>
+			<hgroup>
+				<h1>Sign in</h1>
+				<h2>A minimalist layout for Login pages</h2>
+			</hgroup>
+			<form>
+				<input type="text" name="login" placeholder="Login" aria-label="Login" autocomplete="nickname" required>
+				<input type="password" name="password" placeholder="Password" aria-label="Password"
+				       autocomplete="current-password" required>
+				<fieldset>
+					<label for="remember">
+						<input type="checkbox" role="switch" id="remember" name="remember">
+						Remember me
+					</label>
+				</fieldset>
+				<button type="submit" class="contrast" onclick="event.preventDefault()">Login</button>
+			</form>
+		</div>
+		<div></div>
+	</article>
+</main><!-- ./ Main -->
+
+      <!-- Footer -->
+<footer class="container-fluid">
+	<small>Built with <a href="https://picocss.com" class="secondary">Pico</a> • <a
+			href="https://github.com/picocss/examples/tree/master/sign-in/" class="secondary">Source code</a></small>
+</footer><!-- ./ Footer -->
