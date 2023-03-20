aad15f812613771abd12781acd94f9576ca8bb55
ynr <ya_onur@hotmail.com>
2023-03-20 08:36:16 +0300

    ss

---
+++ svelte/ali/s7-stores/postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    "tailwindcss/nesting": "postcss-nesting",
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+  // plugins: [require("autoprefixer"), require("postcss-nested")],
---
+++ sveltekit/ecom_skeleton/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+import ProductCard from "../lib/ProductCard.svelte";
+
+const products=[
+
+]
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+  <div class="grid grid-cols-3 gap-4">
+    <div class="col-span-3">
+      <h1>Sveltik store</h1>
+    </div>
+      <ProductCard></ProductCard>
+    <div class="col-span-3">
+      <button class="btn variant-filled-primary">Checkout with Stripe Api</button>
+    </div>
+  </div>
+</div>
