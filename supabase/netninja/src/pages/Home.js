ded548744c3f9d8e352987a034f5f762b04340e5
ynr <ya_onur@hotmail.com>
2023-03-23 04:20:19 +0300

    supabase setup

--- supabase/netninja/src/pages/Home.js
+++ supabase/netninja/src/pages/Home.js
@@ -1,4 +1,10 @@
+import supabase from "../config/supabaseClient"
+
+
+
 const Home = () => {
+  console.log("inside home")
+  console.log(supabase)
   return (
     <div className="page home">
       <h2>Home</h2>
