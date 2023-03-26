9b535005c063f89d90fbe67eec6b2b2cbd79e715
ynr <ya_onur@hotmail.com>
2023-03-26 06:34:51 +0300

    ss

--- supabase/jamesq/src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-import { PUBLIC_SUPABASE_URL,PUBLIC_ANON_KEY } from "$env/static/public"
-
-// Create a single supabase client for interacting with your database
-export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_ANON_KEY)
